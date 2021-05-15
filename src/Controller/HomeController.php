79e54462cd43a91e1800a8fc6d44fd75950addaf
rayen hidri <rayen.hidri@gmail.com>
2021-05-15 22:02:50 +0200

    ajout liste categ dans accueil

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Entity\Category;
 use App\Repository\ArticleRepository;
+use App\Repository\CategoryRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,10 +13,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class HomeController extends AbstractController
 {
     private $repoArticle;
+    private $repoCategory;
 
-    public function __construct(ArticleRepository $repoArticle)
+    public function __construct(ArticleRepository $repoArticle, CategoryRepository $repoCategory)
     {
         $this->repoArticle = $repoArticle;
+        $this->repoCategory = $repoCategory;
     }
 
     /**
@@ -22,9 +26,11 @@ class HomeController extends AbstractController
      */
     public function index(): Response
     {
+        $categories = $this->repoCategory->findAll();
         $articles = $this->repoArticle->findAll();
         return $this->render('home/index.html.twig', [
-            "articles" => $articles
+            "articles" => $articles,
+            "categories" => $categories
         ]);
     }
 
@@ -38,6 +44,24 @@ class HomeController extends AbstractController
         }
         return $this->render('show/index.html.twig', [
             "article" => $article
+        ]);
+    }
+
+    /**
+     * @Route("/showArticles/{id}", name="show_article")
+     */
+    public function showArticles(?Category $category): Response
+    {
+        if($category)
+        {
+            $articles = $category->getArticles()->getValues();
+        }
+        else {
+            return $this->redirectToRoute("home");
+        }
+        return $this->render('home/index.html.twig', [
+            "articles" => $articles,
+            "categories" => $this->repoCategory->findAll()
         ]);
     }
 }
